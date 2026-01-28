d55a4fd704cdf91bb48fa82737827105878e4f5f
Fernando Mendoza <fernandogx8647@gmail.com>
2026-01-28 16:59:52 -0600

    BlockMovement & ObstacleLayer

--- Assets/Scripts/RollingMovement.cs
+++ Assets/Scripts/RollingMovement.cs
@@ -6,19 +6,41 @@ using UnityEngine.InputSystem;
 public class RollingMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] LayerMask obstacleLayer;
     private bool isMoving = false;
 
     void Update()
     {
         if (isMoving) return;
 
-        if (Input.GetKeyDown(KeyCode.W)) StartCoroutine(Move(Vector3.forward));
-        else if (Input.GetKeyDown(KeyCode.S)) StartCoroutine(Move(Vector3.back));
-        else if (Input.GetKeyDown(KeyCode.A)) StartCoroutine(Move(Vector3.left));
-        else if (Input.GetKeyDown(KeyCode.D)) StartCoroutine(Move(Vector3.right));
+        if (Input.GetKeyDown(KeyCode.W)) TryMove(Vector3.forward);
+        else if (Input.GetKeyDown(KeyCode.S)) TryMove(Vector3.back);
+        else if (Input.GetKeyDown(KeyCode.A)) TryMove(Vector3.left);
+        else if (Input.GetKeyDown(KeyCode.D)) TryMove(Vector3.right);
     }
 
-    IEnumerator Move (Vector3 direction)
+    void TryMove(Vector3 direction)
+    {
+        if (!IsBlocked(direction))
+        {
+            StartCoroutine(Move(direction));
+        }
+    }
+
+    bool IsBlocked(Vector3 direction)
+    {
+
+        if (Physics.Raycast(transform.position, direction, out RaycastHit hit, 1f, obstacleLayer))
+        {
+            Debug.DrawRay(transform.position, direction * 1f, Color.red, 1f);
+            Debug.Log("Blocked by " + hit.collider.name);
+            return true;
+        }
+        Debug.DrawRay(transform.position, direction * 1f, Color.green, 1f);
+        return false;
+    }
+
+    IEnumerator Move(Vector3 direction)
     {
         isMoving = true;
 
@@ -35,7 +57,18 @@ public class RollingMovement : MonoBehaviour
             remainingAngle -= angleToRotate;
             yield return null;
         }
-        
+
+        RoundPosition();
+
         isMoving = false;
+    }
+
+    void RoundPosition()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Round(pos.x);
+        pos.y = 0.5f;
+        pos.z = Mathf.Round(pos.z);
+        transform.position = pos;
     }
 }

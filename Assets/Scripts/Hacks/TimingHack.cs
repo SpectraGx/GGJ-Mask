d98597d719c7d39aba6114e3825dc8df24f382b9
Fernando Mendoza <fernandogx8647@gmail.com>
2026-01-29 11:09:57 -0600

    Minigame TimingHack

---
+++ Assets/Scripts/Hacks/TimingHack.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimingHack : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] RectTransform needle;
+    [SerializeField] Image targetZone;
+    [SerializeField] Transform targetZoneTransform;
+
+    [Header("Settings")]
+    [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private float successAngleTolerance = 0.25f;
+    [SerializeField] private int stagesToComplete = 3;
+
+    private int currentState = 0;
+    private bool isRunning = false;
+    private float currentZoneAngle;
+
+    private HackMinigameController hackController;
+
+    void Start()
+    {
+        hackController = GetComponentInParent<HackMinigameController>();
+    }
+
+    void OnEnable()
+    {
+        ResetMinigame();
+    }
+
+    void Update()
+    {
+        if (!isRunning) return;
+        needle.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckNeedlePosition();
+        }
+    }
+
+    void CheckNeedlePosition()
+    {
+        float needleAngle = NormalizeAngle(needle.localEulerAngles.z + 180f);
+        float zoneStart = NormalizeAngle(currentZoneAngle);
+        //float zoneEnd = NormalizeAngle(currentZoneAngle - (targetZone.fillAmount * 360f));
+        float zoneCenter = currentZoneAngle - (targetZone.fillAmount*360f/2f);
+
+        float angleDiff = Mathf.Abs(Mathf.DeltaAngle(needleAngle, zoneCenter));
+        float tolerance = (targetZone.fillAmount * 360f) / 2f;
+        
+        if (angleDiff < tolerance)
+        {
+            Success();
+        }
+        else
+        {
+            Fail();
+        }
+    }
+
+    void Success()
+    {
+        Debug.Log("ACIERO" + currentState);
+        currentState++;
+        rotationSpeed *= 1.5f;
+        rotationSpeed *= -1;
+
+        if (currentState >= stagesToComplete)
+        {
+            WinGame();
+        }
+        else
+        {
+            NextRound();
+        }
+    }
+
+    void Fail()
+    {
+        Debug.Log("ERROR EN" + currentState);
+        ResetMinigame();
+    }
+
+    void WinGame()
+    {
+        Debug.Log("Minigame Vencido!");
+        hackController.CompleteHacking();
+    }
+
+    void NextRound()
+    {
+        float randomAngle = Random.Range(0f, 360f);
+        targetZoneTransform.localRotation = Quaternion.Euler(0, 0, randomAngle);
+        currentZoneAngle = randomAngle;
+    }
+
+    void ResetMinigame()
+    {
+        currentState = 0;
+        rotationSpeed = 200f;
+        isRunning = true;
+        NextRound();
+    }
+
+    float NormalizeAngle(float angle)
+    {
+        angle = angle % 360f;
+        if (angle < 0) angle += 360f;
+        return angle;
+    }
+
+}

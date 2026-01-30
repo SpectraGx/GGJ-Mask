fb68746cf6cb689f5f78a42a40eb43758ee228fc
Fernando Mendoza <fernandogx8647@gmail.com>
2026-01-29 19:39:52 -0600

    TraceManager & ColorPlayerMask

---
+++ Assets/Scripts/HUD/TraceManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TraceManager : MonoBehaviour
+{
+    public static TraceManager Instance;
+
+    [Header("Trace Settings")]
+    [SerializeField] private float maxTrace = 100f;
+    [SerializeField] private float passiveFillRate = 1.5f;
+    [SerializeField] private float penaltyAmount = 15f;
+    [SerializeField] private float rewardAmount = 10f;
+
+    [Header("UI")]
+    [SerializeField] private Slider traceSlider;
+    [SerializeField] private Image fillImage;
+    [SerializeField] private Color safeColor = Color.green;
+    [SerializeField] private Color criticalColor = Color.red;
+
+    private float currentTrace;
+    private bool isTraceActive = true;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        
+    }
+
+    void Start()
+    {
+        currentTrace = maxTrace;
+        UpdateTraceUI();
+    }
+
+    void Update()
+    {
+        if (!isTraceActive) return;
+
+        if (GameManager.instance.currentState == GameState.GameOver) return;
+
+        currentTrace -= passiveFillRate * Time.deltaTime;
+
+        if (currentTrace <= 0)
+        {
+            currentTrace = 0;
+            TriggerSystemPurge();
+        }
+
+        UpdateTraceUI();
+    }
+
+    public void ModifyTrace(float amount)
+    {
+        currentTrace += amount;
+        currentTrace = Mathf.Clamp(currentTrace, 0, maxTrace);
+        UpdateTraceUI();
+    }
+
+    private void UpdateTraceUI()
+    {
+        traceSlider.value = currentTrace / maxTrace;
+        if (currentTrace / maxTrace < 0.3f)
+        {
+            fillImage.color = Color.Lerp(criticalColor, safeColor, (currentTrace / maxTrace) / 0.3f);
+        }
+        else
+        {
+            fillImage.color = safeColor;
+        }
+    }
+
+    void TriggerSystemPurge()
+    {
+        isTraceActive = false;
+        Debug.Log("System Purge Triggered!");
+        FindObjectOfType<PlayerHealth>().TakeDamage();
+    }
+
+}
--- Assets/Scripts/Player/PlayerMaskManager.cs
+++ Assets/Scripts/Player/PlayerMaskManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum MaskType { None, FireWall, Admin, Guest}
+public enum MaskType { None, FireWall, Admin, Guest }
 
 public class PlayerMaskManager : MonoBehaviour
 {
@@ -15,6 +16,14 @@ public class PlayerMaskManager : MonoBehaviour
     [SerializeField] private Material adminMaterial;
     [SerializeField] private Material guestMaterial;
 
+    [Header("Mask UI")]
+    [SerializeField] private Image maskIconUI;
+    [SerializeField] private Color noneColor;
+    [SerializeField] private Color fireWallColor;
+    [SerializeField] private Color adminColor;
+    [SerializeField] private Color guestColor;
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) EquipMask(MaskType.FireWall);
@@ -26,10 +35,24 @@ public class PlayerMaskManager : MonoBehaviour
     void EquipMask(MaskType type)
     {
         currentMask = type;
-        switch (type){ case MaskType.None: cubeRenderer.material = noneMaterial; break;
-            case MaskType.FireWall: cubeRenderer.material = fireWallMaterial; break;
-            case MaskType.Admin: cubeRenderer.material = adminMaterial; break;
-            case MaskType.Guest: cubeRenderer.material = guestMaterial; break;
+        switch (type)
+        {
+            case MaskType.None: 
+                cubeRenderer.material = noneMaterial;
+                maskIconUI.color = noneColor; 
+                break;
+            case MaskType.FireWall: 
+                cubeRenderer.material = fireWallMaterial; 
+                maskIconUI.color = fireWallColor;
+                break;
+            case MaskType.Admin: 
+                cubeRenderer.material = adminMaterial; 
+                maskIconUI.color = adminColor;
+                break;
+            case MaskType.Guest: 
+                cubeRenderer.material = guestMaterial; 
+                maskIconUI.color = guestColor;
+                break;
         }
         Debug.Log("Equipped Mask: " + type.ToString());
     }
